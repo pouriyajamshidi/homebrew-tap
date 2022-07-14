8f2436b9be8c0e8b7ab0ed5432560cdd48b055ba
icemint0828 <icemint0828@gmail.com>
2022-07-14 21:50:17 +0900

    update to v1.12.1

--- tcping.rb
+++ tcping.rb
@@ -1,9 +1,9 @@
-HOMEBREW_CLI_VERSION='1.9.0'
+HOMEBREW_CLI_VERSION='1.12.1'
 class Tcping < Formula
   desc "Ping TCP ports. Inspired by Linux's ping utility. Written in Go"
   homepage "https://github.com/pouriyajamshidi/tcping"
   url "https://github.com/pouriyajamshidi/tcping/releases/download/v#{HOMEBREW_CLI_VERSION}/tcping_MacOS.zip"
-  sha256 "9c3983d08a263a02563280cd11899198fc9d1ac626a137896aea9d5dc8fdd752"
+  sha256 "5fe89401fdbfe269760bcc1491896bf8c5b441add0877acf13cb957a4ac1759b"
   license "MIT"
   version "v#{HOMEBREW_CLI_VERSION}"
 
